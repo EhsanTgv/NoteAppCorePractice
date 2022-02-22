1f7ebdf408117e9ddddc410d64dfd13476d88557
EhsanTgv <EhsanTgv@gmail.com>
2022-02-22 17:23:04 +0300

    Initial Commit

---
+++ NoteAppCorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteAppCorePractice
+//
+//  Created by İbrahim on 22.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
