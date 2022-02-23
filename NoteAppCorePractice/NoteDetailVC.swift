98c0a4016d7d953f7bdc7bef6a7a84462781a49e
EhsanTgv <EhsanTgv@gmail.com>
2022-02-23 17:49:18 +0300

    Creating view classes

---
+++ NoteAppCorePractice/NoteDetailVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NoteAppCorePractice
+//
+//  Created by İbrahim on 22.02.2022.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+    @IBOutlet weak var titleTF: UITextField!
+    @IBOutlet weak var descriptionTF: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveAction(_ sender: Any) {
+    }
+    
+}
+
